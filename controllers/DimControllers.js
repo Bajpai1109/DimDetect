3d73c67a3acb61965b97642e9c0735e360c54bbc
Aditya Goyal <goyaladitya2212@gmail.com>
2024-11-22 03:55:54 +0530

    fetch esp data

--- controllers/DimControllers.js
+++ controllers/DimControllers.js
@@ -1,20 +1,40 @@
+import axios from "axios";
 import { db } from "../config/firebaseConfig.js";
 
-
 export const getDis = async (request, response, next) => {
-    try {
-        const docRef = db.collection("distance").doc("001");
-        const doc = await docRef.get();
+	try {
+		const docRef = db.collection("distance").doc("001");
+		const doc = await docRef.get();
 
-        if(!doc.exists){
-            return response.status(404).json({ message: "data not found" });
-        }
+		console.log("heloo");
 
-        const docData = await doc.data();
-        console.log(docData);
-        return response.status(200).json({docData});
-    } catch (error){
-        console.error(error);
-        return response.status(500).json({ message: "Error fetching data" });
-    }
-}
+		if (!doc.exists) {
+			return response.status(404).json({ message: "data not found" });
+		}
+
+		const docData = await doc.data();
+		console.log(docData);
+
+		const baseUrl = docData.url;
+		const res = await axios.get(baseUrl, {
+			headers: { "Content-Type": "application/json" },
+		});
+
+		const responseData = res.data.data;
+		console.log(responseData);
+
+		await docRef.set(
+			{
+				dis: responseData.dis,
+			},
+			{ merge: true }
+		);
+
+		return response
+			.status(200)
+			.json({ data: { dis: responseData.dis }, message: "success" });
+	} catch (error) {
+		console.error(error);
+		return response.status(500).json({ message: "Error fetching data" });
+	}
+};
